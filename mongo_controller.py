bb6a94567385281dd6bb4a38c106d94c080dc8a9
dubiousmane <dubiousmane@gmail.com>
2024-11-08 23:43:36 +0500

    added frontend on flask and added new controller functions

--- mongo_controller.py
+++ mongo_controller.py
@@ -2,14 +2,26 @@ from database import get_database
 
 # Получаем объект базы данных
 db = get_database()
-collection = db["users"]
+
 
 def add_user(user_info):
+    collection = db["resume"]
     result = collection.insert_one(user_info)
     return str(result.inserted_id)
 
 def get_all_users():
+    collection = db["resume"]
     users = list(collection.find())
     return users
+
+def get_all_vacancies():
+    collection = db["vacancies"]
+    users = list(collection.find())
+    return users
+
+def add_vacancy(vacancy):
+    collection = db["vacancies"]
+    result = collection.insert_one(vacancy)
+    return str(result.inserted_id)
 
 
